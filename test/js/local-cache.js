9ac6ff959313f952200b358f396ec1ec2c364f27
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-10 10:12:09 +0200

    init

---
+++ test/js/local-cache.js
@@ -0,0 +1,288 @@
+/*global LocalCache */
+QUnit.module('LocalCache', {
+  setup: function() {
+    var config = this.config = {};
+    function keys(obj) {
+      var ret = [];
+      for (var name in obj) {
+        if (obj.hasOwnProperty(name)) {
+          ret.push(name);
+        }
+      }
+      return ret;
+    }
+    this.storage = {
+      get length() {
+        return keys(config).length;
+      },
+      key: this.spy(function(i) { return keys(config)[i]; }),
+      getItem: this.spy(function(name) { return config[name]; }),
+      setItem: this.spy(function(name, value) { config[name] = value; }),
+      removeItem: this.spy(function(name) { delete config[name]; })
+    };
+
+    this.cache = LocalCache;
+    LocalCache = LocalCache.constructor(this.storage);
+
+    // Clear out counters from the quota testing
+    this.storage.setItem.reset();
+    this.storage.removeItem.reset();
+  },
+  teardown: function() {
+    LocalCache = this.cache;
+  }
+});
+
+test('value stored - no ttl', function() {
+  LocalCache.store('test', 'test');
+  QUnit.equal(this.storage.setItem.callCount, 1);
+
+  QUnit.equal(LocalCache.get('test'), 'test');
+  QUnit.equal(this.storage.getItem.callCount, 2);
+  QUnit.equal(this.storage.setItem.callCount, 1);
+});
+test('value stored - hour ttl', function() {
+  LocalCache.store('test', 'test', LocalCache.TTL.HOUR);
+  QUnit.equal(this.storage.setItem.callCount, 3);
+
+  QUnit.equal(LocalCache.get('test'), 'test');
+  QUnit.equal(this.storage.getItem.callCount, 2);
+  QUnit.equal(this.storage.setItem.callCount, 4);
+});
+
+test('ttl expired', function() {
+  LocalCache.store('test', 'test', LocalCache.TTL.HOUR);
+  QUnit.equal(this.storage.setItem.callCount, 3);
+
+  this.clock.tick(LocalCache.TTL.HOUR()+1);
+
+  QUnit.equal(LocalCache.get('test'), undefined);
+  QUnit.equal(this.storage.setItem.callCount, 3);
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+});
+
+test('remove - no ttl', function() {
+  LocalCache.store('test', 'test');
+  ok(this.storage.setItem.calledOnce);
+
+  LocalCache.remove('test');
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+
+  QUnit.equal(LocalCache.get('test'), undefined);
+  QUnit.equal(this.storage.getItem.callCount, 2);
+  QUnit.equal(this.storage.setItem.callCount, 1);
+});
+test('remove - hour ttl', function() {
+  // Use something other than zero as that's all we'll see in the real world(tm)
+  this.clock.tick(10);
+
+  LocalCache.store('test', 'test', LocalCache.TTL.HOUR);
+  QUnit.equal(this.storage.setItem.callCount, 3);
+
+  LocalCache.remove('test');
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+
+  QUnit.equal(LocalCache.get('test'), undefined);
+  QUnit.equal(this.storage.getItem.callCount, 2);
+  QUnit.equal(this.storage.setItem.callCount, 3);
+});
+
+test('reset', function() {
+  LocalCache.store('test', 'test');
+  LocalCache.store('test2', 'test', LocalCache.TTL.HOUR);
+  LocalCache.reset();
+
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+  QUnit.equal(LocalCache.get('test'), 'test');
+});
+test('reset - hard', function() {
+  LocalCache.store('test', 'test');
+  LocalCache.store('test2', 'test', LocalCache.TTL.HOUR);
+  LocalCache.reset(true);
+  QUnit.deepEqual(this.config, {});
+});
+
+test('invalidate', function() {
+  LocalCache.store('foo', 'test', LocalCache.TTL.HOUR);
+  LocalCache.store('test', 'test');
+  LocalCache.store('test2', 'test', LocalCache.TTL.HOUR);
+  LocalCache.invalidate('test');
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+  QUnit.equal(LocalCache.get('foo'), 'test');
+  QUnit.equal(LocalCache.get('test'), 'test');
+  QUnit.equal(LocalCache.get('test2'), undefined);
+});
+
+test('invalidate - hard', function() {
+  LocalCache.store('foo', 'test', LocalCache.TTL.HOUR);
+  LocalCache.store('test', 'test');
+  LocalCache.store('test2', 'test', LocalCache.TTL.HOUR);
+  LocalCache.invalidate('test', true);
+  QUnit.equal(this.storage.removeItem.callCount, 6);
+  QUnit.equal(LocalCache.get('foo'), 'test');
+  QUnit.equal(LocalCache.get('test'), undefined);
+  QUnit.equal(LocalCache.get('test2'), undefined);
+});
+
+test('flush expired', function() {
+  LocalCache.store('test', 'test', LocalCache.TTL.hours(0.5));
+  LocalCache.store('test2', 'test', LocalCache.TTL.WEEK);
+  LocalCache.store('test3', 'test');
+  QUnit.equal(this.storage.setItem.callCount, 7);
+  QUnit.equal(this.storage.getItem.callCount, 0);
+
+  this.clock.tick(LocalCache.TTL.HOUR()+1);
+
+  QUnit.equal(this.storage.getItem.callCount, 4); // Flush runs twice
+  QUnit.equal(this.storage.setItem.callCount, 7);
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+  QUnit.equal(LocalCache.get('test2'), 'test');
+  QUnit.equal(LocalCache.get('test3'), 'test');
+});
+
+test('zero quota should not throw', function() {
+  this.storage.setItem = this.spy(function() { var err = new Error(); err.name = 'QUOTA_EXCEEDED_ERR'; throw err; });
+
+  ok(!LocalCache.store('test', 'test', LocalCache.TTL.hours(0.5)));
+  ok(!LocalCache.store('test2', 'test', LocalCache.TTL.WEEK));
+  ok(!LocalCache.store('test3', 'test'));
+
+  QUnit.equal(this.storage.setItem.callCount, 3);
+});
+
+test('quota with no expires', function() {
+  var self = this;
+
+  this.storage.setItem = this.spy(function(name, value) {
+    if (self.storage.length > 0) {
+      var err = new Error();
+      err.name = 'QUOTA_EXCEEDED_ERR';
+      throw err;
+    } else {
+      self.config[name] = value;
+    }
+  });
+
+  ok(LocalCache.store('test', 'test'));
+  QUnit.equal(this.storage.removeItem.callCount, 0);
+
+  ok(!LocalCache.store('test2', 'test'));
+
+  QUnit.equal(this.storage.setItem.callCount, 2);
+  QUnit.equal(this.storage.removeItem.callCount, 0);
+
+  QUnit.equal(LocalCache.get('test'), 'test');
+  QUnit.equal(LocalCache.get('test2'), undefined);
+});
+test('quota with expires', function() {
+  var self = this;
+
+  this.storage.setItem = this.spy(function(name, value) {
+    if (self.storage.length > 2) {
+      var err = new Error();
+      err.name = 'QUOTA_EXCEEDED_ERR';
+      throw err;
+    } else {
+      self.config[name] = value;
+    }
+  });
+
+  ok(LocalCache.store('test', 'test', LocalCache.TTL.HOUR));
+  QUnit.equal(this.storage.removeItem.callCount, 0);
+
+  ok(LocalCache.store('test2', 'test'));
+
+  QUnit.equal(this.storage.setItem.callCount, 5);
+  QUnit.equal(this.storage.removeItem.callCount, 3);
+
+  QUnit.equal(LocalCache.get('test'), undefined);
+  QUnit.equal(LocalCache.get('test2'), 'test');
+});
+test('quota removing all expires but still lacking space', function() {
+  LocalCache.store('test', 'test', LocalCache.TTL.hours(0.5));
+  LocalCache.store('test2', 'test', LocalCache.TTL.WEEK);
+  QUnit.equal(this.storage.setItem.callCount, 6);
+
+  this.storage.setItem = this.spy(function() { var err = new Error(); err.name = 'QUOTA_EXCEEDED_ERR'; throw err; });
+
+  ok(!LocalCache.store('test3', 'test'));
+  QUnit.equal(this.storage.removeItem.callCount, 6);
+  QUnit.equal(this.storage.setItem.callCount, 3);
+
+  QUnit.equal(LocalCache.get('test'), undefined);
+  QUnit.equal(LocalCache.get('test2'), undefined);
+  QUnit.equal(LocalCache.get('test3'), undefined);
+});
+
+test('existing get in private mode', function() {
+  ok(LocalCache.store('test2', 'test', LocalCache.TTL.WEEK));
+  QUnit.equal(this.storage.setItem.callCount, 3);
+
+  this.storage.setItem = this.spy(function() { var err = new Error(); err.name = 'QUOTA_EXCEEDED_ERR'; throw err; });
+
+  QUnit.equal(LocalCache.get('test2'), 'test');
+});
+
+test('private failover', function() {
+  var storage = {
+    setItem: this.spy(function(name, value) { throw new Error(); })
+  };
+
+  var cache = LocalCache.constructor(storage);
+  cache.store('test', 'test');
+  QUnit.equal(cache.get('test'), 'test');
+
+  cache.store('test2', 'test', cache.TTL.HOUR);
+  QUnit.equal(cache.get('test2'), undefined);
+
+  QUnit.equal(storage.setItem.callCount, 1);
+});
+test('quota failover recovery', function() {
+  var originalSet = this.storage.setItem,
+      storage = this.storage;
+  this.storage.setItem = this.spy(function() {
+    // Quota should die once
+    storage.setItem = originalSet;
+
+    var err = new Error();
+    err.name = 'QUOTA_EXCEEDED_ERR';
+    throw err;
+  });
+
+  var cache = LocalCache.constructor(storage);
+  cache.store('test', 'test');
+  QUnit.equal(cache.get('test'), 'test');
+
+  cache.store('test2', 'test', cache.TTL.HOUR);
+  QUnit.equal(cache.get('test2'), 'test');
+
+  QUnit.equal(storage.setItem.callCount, 6);
+});
+
+QUnit.module('TTL');
+
+test('day', function() {
+  var start = new Date('01 Jan 2014 02:00:00 GMT-0800 (PST)'),
+      expected = new Date('02 Jan 2014 02:00:00 GMT-0800 (PST)');
+
+  this.clock.now = start.getTime();
+
+  for (var i = 0; i < 24*60*60*1000; i += 799000) {
+    this.clock.now = start.getTime() + i;
+    QUnit.equal(LocalCache.TTL.DAY(), expected.getTime());
+  }
+  this.clock.restore();
+});
+
+test('hour', function() {
+  var start = new Date('01 Jan 2014 02:00:00 GMT-0800 (PST)'),
+      expected = new Date('01 Jan 2014 03:00:00 GMT-0800 (PST)');
+
+  this.clock.now = start.getTime();
+
+  for (var i = 0; i < 60*60*1000; i += 79900) {
+    this.clock.now = start.getTime() + i;
+    QUnit.equal(LocalCache.TTL.HOUR(), expected.getTime());
+  }
+  this.clock.restore();
+});
